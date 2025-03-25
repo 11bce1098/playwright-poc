9abba47eee8efb753ad258ce0d97f432c87ad844
11bce1098 <161597500+11bce1098@users.noreply.github.com>
2025-03-25 13:32:10 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -16,7 +16,7 @@ const config = {
   globalSetup: require.resolve("./config/globalSetup"),
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 90 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
